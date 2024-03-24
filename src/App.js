813d0c6270fba16c43d5f81c06f1c750c8b4c098
nirdesh1234 <35981417+nirdesh1234@users.noreply.github.com>
2024-03-24 19:58:52 +0545

    persistence with json server minor updates and refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+    //console.log(response);
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data }; //merge 2 properties together
       }
 
       return book;
@@ -17,7 +30,8 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -25,14 +39,11 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: title,
+    });
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
@@ -46,3 +57,9 @@ function App() {
 }
 
 export default App;
+
+// { ...response.data } creates a new object containing all the properties and values from response.data.
+// { ...book, ...response.data } merges the properties of the book object with the properties from response.data.
+//If there are any overlapping properties, the ones from response.data will overwrite the ones from book.
+// By using this spread syntax, only the properties from response.data that need to be updated or added to the book object are changed,
+// while leaving other properties intact.
